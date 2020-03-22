a1eb4a1f9049c68d0ae6759c5598cbc85155d29f
foamydave <42722340+foamydave@users.noreply.github.com>
2020-03-22 16:04:16 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -1,3 +1,21 @@
+/*
+JavaScript Code
+===============
+
+This is the JavaScript file where all your code will live. You can start by 
+writing your code in any one of the functions below. You can add functions as
+well. 
+
+To see your output from your JavaScript code, double click/launch output.html
+to see it in a browser. If you update the code in this file then refresh the 
+browser page to the see the new results.
+
+The 3 functions below work as follows:
+	1) Runs as soon as you open the accompaning output.html file in a browser.
+	2) Runs after you press the Submit 2 button.
+	3) Runs after you enter text in Input 3 and press the Submit 3 button.
+*/
+
 
 function Function1() {
 	var x =	"Auto-runs when page loads.\n It also honors newline (\\n).";
